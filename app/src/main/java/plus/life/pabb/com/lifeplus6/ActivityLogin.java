cf20c1ec833ea57f2f0673e7101a3b1d84b02610
Litmeister <38861881+Litmeister@users.noreply.github.com>
2018-09-11 00:46:52 -0300

    Add files via upload
    Version con la inclucion de la ficha medica

--- app/src/main/java/plus/life/pabb/com/lifeplus6/ActivityLogin.java
+++ app/src/main/java/plus/life/pabb/com/lifeplus6/ActivityLogin.java
@@ -19,7 +19,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class ActivityLogin extends AppCompatActivity {
     public EditText loginEmailId, logInpasswd;
     Button btnLogIn;
-    TextView signup;
+    TextView signup, textView5;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
 
@@ -32,6 +32,7 @@ public class ActivityLogin extends AppCompatActivity {
         logInpasswd = findViewById(R.id.loginpaswd);
         btnLogIn = findViewById(R.id.btnLogIn);
         signup = findViewById(R.id.TVSignIn);
+        textView5 = findViewById(R.id.textView5);
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
--- app/src/main/java/plus/life/pabb/com/lifeplus6/UserActivity.java
+++ app/src/main/java/plus/life/pabb/com/lifeplus6/UserActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserActivity extends AppCompatActivity {
-    Button btnLogOut;
+    Button btnLogOut, button, button2, button3, button4;
+    ProgressBar progressBar, progressBar2, progressBar3, progressBar4;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
 
@@ -18,6 +20,15 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
+        progressBar3 = (ProgressBar) findViewById(R.id.progressBar3);
+        progressBar4 = (ProgressBar) findViewById(R.id.progressBar4);
+
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,5 +40,40 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(UserActivity.this, Medicamentos.class);
+                startActivity(I);
+            }
+
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(UserActivity.this, Tratamiento.class);
+                startActivity(I);
+            }
+
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(UserActivity.this, Ejercicios.class);
+                startActivity(I);
+            }
+
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(UserActivity.this, ConsultasMedicas.class);
+                startActivity(I);
+            }
+
+        });
     }
 }
